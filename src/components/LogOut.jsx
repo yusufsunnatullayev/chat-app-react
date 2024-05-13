710a6698435dd64c20726e2c840959509651ee50
Yusuf Sunnatullayev <yusuffdeveloper@gmail.com>
2024-05-13 18:59:49 +0500

    few changes

--- src/components/LogOut.jsx
+++ src/components/LogOut.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
-import { auth } from '../firebase'
+import {auth} from '../firebase'
 
 const style = {
-    button: `bg-red-800 px-4 py-2 hover:bg-red-700`
+    button: `bg-gray-200 px-4 py-2 hover:bg-gray-100`
 }
 
+
 const LogOut = () => {
+const signOut = () => {
+    signOut(auth)
+}
 
-    const signOut = () => {
-        signOut(auth)
-    }
-
-    return (
-        <button onClick={() => auth.signOut()} className={style.button}>
-            Log Out
-        </button>
-    )
+  return (
+    <button onClick={() => auth.signOut()} className={style.button}>
+        Logout
+    </button>
+  )
 }
 
 export default LogOut
